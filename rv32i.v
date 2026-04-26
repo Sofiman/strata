3d8586e98d7b744039292ce1e2c58c69e74ac06c
Sofiane <sofiane.meftah@outlook.fr>
2026-04-26 23:14:35 +0200

    refactor: explicit fetch stall state
    Rewrite the fetch stall logic to be simpler and easier
    to adapt and use with future CPU features.

--- rv32i.v
+++ rv32i.v
@@ -7,15 +7,14 @@ module rv32i (
 );
 
     // ROM
-    wire data_valid_next;
-    reg data_valid;
+    wire data_valid;
 
     // Fetch
     reg [31:0] pc;
-    reg pc_next;
+    reg [31:0] pc_next;
     reg [31:0] pc_to_load;
-    reg [31:0] inst;
-    wire [31:0] inst_next;
+    reg [31:0] pc_to_load_next;
+    wire [31:0] inst;
 
     // ALU
     reg [2:0] alu_op;
@@ -44,9 +43,9 @@ module rv32i (
     rom inst_mem (
         .rst(rst),
         .clk(clk),
-        .addr(pc_to_load[31:2]),
-        .data(inst_next),
-        .data_valid(data_valid_next)
+        .addr(pc_to_load[10:2]),
+        .data(inst),
+        .data_valid(data_valid)
     );
 
     register_file rf (
@@ -93,16 +92,18 @@ module rv32i (
 
     localparam S_FETCH_DECODE = 0;
     localparam S_EXECUTE      = 1;
-    localparam STATE_BITS = $clog2(S_EXECUTE + 1);
-    reg [STATE_BITS-1:0] state;
-    reg [STATE_BITS-1:0] state_next;
+    localparam S_FETCH_STALL  = 2;
+    localparam STATE_BITS = $clog2(S_FETCH_STALL + 1);
+    reg [1:0] state;
+    reg [1:0] state_next;
 
     `ifdef BENCH
     reg [127:0] _b__state_name;
     always @(*) begin
         case (state)
-            S_FETCH_DECODE: _b__state_name <= "S_FETCH_DECODE";
-            S_EXECUTE     : _b__state_name <= "S_EXECUTE";
+            S_FETCH_DECODE: _b__state_name <= "FETCH_DECODE";
+            S_EXECUTE     : _b__state_name <= "EXECUTE";
+            S_FETCH_STALL : _b__state_name <= "FETCH_STALL";
         endcase
     end
     `endif
@@ -110,7 +111,9 @@ module rv32i (
     `include "cfg/rv_isa_opcode.v"
 
     always @(*) begin
-        pc_to_load <= pc + 4;
+        pc_to_load_next <= pc + 4;
+        pc_next <= pc;
+
         wr__en <= 0;
 
         alu_op <= funct3;
@@ -147,14 +150,14 @@ module rv32i (
                 alu_op <= /* ADD */ 0;
                 alu_a <= pc;
                 alu_b <= 4;
-                pc_to_load <= pc + uj_imm;
+                pc_to_load_next <= pc + uj_imm;
             end
             JALR: begin
                 // rd = PC + 4;
                 alu_op <= /* ADD */ 0;
                 alu_a <= pc;
                 alu_b <= 4;
-                pc_to_load <= rf_a + i_imm; // TODO: Set the LSB to 0 (page 31)
+                pc_to_load_next <= rf_a + i_imm; // TODO: Set the LSB to 0 (page 31)
             end
             default: begin
                 // TODO
@@ -162,40 +165,43 @@ module rv32i (
         endcase
 
         state_next <= state;
-        pc_next <= 0;
         case (state)
             S_FETCH_DECODE: begin
-                if (data_valid) begin
-                    state_next <= S_EXECUTE;
-                end else begin
-                    pc_to_load <= pc;
-                end
+                state_next <= S_EXECUTE;
             end
             S_EXECUTE: begin
                 state_next <= S_FETCH_DECODE;
-                pc_next <= 1;
+                pc_next <= pc_to_load;
 
                 case (inst_fmt)
                     R_TYPE, I_TYPE, U_TYPE, J_TYPE: wr__en <= 1;
+                    B_TYPE: begin
+                        state_next <= S_FETCH_STALL;
+                    end
                     default: wr__en <= 0;
                 endcase
+            end
+            S_FETCH_STALL: begin
+                pc_to_load_next <= pc_to_load;
+                pc_next <= pc_to_load;
+                if (data_valid) begin
+                    state_next <= S_FETCH_DECODE;
+                end
             end
         endcase
     end
 
     always @(posedge clk or posedge rst) begin
         if (rst) begin
-            state <= S_FETCH_DECODE;
+            state <= S_FETCH_STALL;
             pc <= 32'h40000000;
+            pc_to_load <= 32'h40000000;
             leds <= 0;
-            inst <= 'h13;
         end else begin
             state <= state_next;
-            if (pc_next) begin
-                pc <= pc_to_load;
-            end
-            inst <= inst_next;
-            data_valid <= data_valid_next;
+
+            pc <= pc_next;
+            pc_to_load <= pc_to_load_next;
 
             leds[0] <= ^alu_out;
         end
