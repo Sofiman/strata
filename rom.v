c9a16d49e907697e20c93a524fbbd9bc380fbce3
Sofiane <sofiane.meftah@outlook.fr>
2026-05-09 21:24:10 +0200

    chore: update rom test programs

--- rom.v
+++ rom.v
@@ -17,14 +17,16 @@ module rom #(
             mem[i] = 'h13;// nop aka addi x0, x0, 0
         end
         `ifdef ROM_FILE
-        $readmemh(`ROM_FILE, mem);
+        $readmemh(`ROM_FILE, mem, 0, DEPTH-1);
         `else
         //$readmemh("testprograms/test_i_type.hex", mem, 0, 3);
         //$readmemh("testprograms/test_ir_type.hex", mem, 0, 4);
         //$readmemh("testprograms/test2_ir_type.hex", mem, 0, 5);
-        $readmemh("testprograms/test3_ir_type.hex", mem, 0, 6);
+        $readmemh("testprograms/ir_type_test2/rom.hex", mem, 0, 6);
         //$readmemh("testprograms/auipc.hex", mem, 0, 2);
-        $readmemh("testprograms/jal.hex", mem, 0+7, 5+7);
+        $readmemh("testprograms/jal/rom.hex", mem, 0+7, 5+7);
+        $readmemh("testprograms/bne/rom.hex", mem, 5+7, 5+7+10);
+        $readmemh("testprograms/bge/rom.hex", mem, 20, 20+10);
         `endif
     end
 
